3f22fec635d5377795fc2801e5ff5d38fd95a95b
Priya Mishra <97019927+Priya-M25@users.noreply.github.com>
2024-02-21 19:22:37 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Asc_no{
+    public static void main(String[] args) {
+        int c=1;
+        for(int i=1; i<=4; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(c +" ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
