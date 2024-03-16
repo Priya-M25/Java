554b6137dc009e7894b092b2d099efe8ca3211a7
Priya Mishra <97019927+Priya-M25@users.noreply.github.com>
2024-03-16 19:33:15 +0530

    Update Right_Half_Num_Pyramid.java

--- Patterns/Right_Half_Num_Pyramid.java
+++ Patterns/Right_Half_Num_Pyramid.java
@@ -1,4 +1,11 @@
 // Java Program to print Right Half Pyramid of a Number
+/*Output
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+*/
 public class Right_Half_Num_Pyramid 
 {
   public static void main(String[] args) 
