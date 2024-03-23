7029591f0c903b6714685bdea9b0ad1cc8eb05fb
Priya Mishra <97019927+Priya-M25@users.noreply.github.com>
2024-03-23 20:18:30 +0530

    Update two_four_wheeler.java

--- two_four_wheeler.java
+++ two_four_wheeler.java
@@ -1,3 +1,21 @@
+/* Problem Statement:
+Write a Java program that calculates the number of two-wheeler and four-wheeler vehicles based on the total number of vehicles and the total number of wheels.
+The program should prompt the user to enter the following inputs:
+1. The number of vehicles (`v`)
+2. The number of wheels (`w`)
+The program should then perform the following checks:
+1. If the number of wheels is less than 2, or if the number of wheels is odd, or if the number of vehicles is greater than the number of wheels, the program should output "Invalid input.....".
+2. Otherwise, the program should calculate and output the number of two-wheeler vehicles and the number of four-wheeler vehicles.
+
+Output:
+Enter the no. of vehicles :- 
+510
+Enter the no. of wheels :- 
+2000
+no. of Two-wheels vehicles :- 20
+no. of Four-wheels vehicles :- 490
+*/
+
 import java.util.Scanner;
 
 public class program_17 {
